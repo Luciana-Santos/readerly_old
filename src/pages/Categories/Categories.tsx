359a9e96c7297dfe71bae01c83d66b5ca3408751
Luciana Santos <luciydss@hotmail.com>
2023-01-18 20:52:53 -0300

    categories page add

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,4 +1,40 @@
+import {
+  AsideCategories,
+  BulletIcon,
+  CategoriesStyled,
+  MainSection,
+} from './Categories.styled';
+import { content } from './content';
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <CategoriesStyled>
+      <AsideCategories>
+        <ul>
+          <li>
+            <a href="#">All</a>
+          </li>
+          {content.map((categorie) => (
+            <li key={categorie.id}>
+              <a href="#">{categorie.categorie}</a>
+            </li>
+          ))}
+        </ul>
+      </AsideCategories>
+
+      <MainSection>
+        <ul>
+          {content.map((categorie) => (
+            <>
+              <li key={categorie.id}>
+                <BulletIcon />
+                <a href="#">{categorie.title}</a>
+              </li>
+            </>
+          ))}
+        </ul>
+      </MainSection>
+    </CategoriesStyled>
+  );
 };
 export default Categories;
